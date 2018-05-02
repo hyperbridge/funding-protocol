4b5cacd0d1111a5c11ed5334412371b7ffc912b0
Jordan Brooks <jordbrooks14@gmail.com>
2018-05-02 13:31:01 -0700

    Initial framework.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Funding Protocol
+
+The Funding Protocol project is broken up into 3 areas:
+
+* Smart Contracts (Ethereum)
+* Communication API (JSON.API)
+* Documentation
---
+++ smart-contracts/ethereum/truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+    contracts_build_directory: "./build",
+    networks: {
+        development: {
+            host: "localhost",
+            port: 7545,
+            network_id: "*" // Match any network id
+        }
+    }
+};
