7fa3ba1599723f6c857e0a3c48cfc11ff4e86d8d
Jordan Brooks <jordbrooks14@gmail.com>
2018-05-14 18:36:41 -0700

    Set up nextjs, react, react dom.

---
+++ .next/5.fe324cf391c1fb2d6e2f.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate(5,{
+
+/***/ "./node_modules/webpack/buildin/harmony-module.js":
+false,
+
+/***/ "./pages/show.js":
+/***/ (function(module, __webpack_exports__) {
+
+"use strict";
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/home/jordan/Projects/hyperbridge/funding-protocol/pages/show.js'");
+
+/***/ })
+
+})
+//# sourceMappingURL=5.fe324cf391c1fb2d6e2f.hot-update.js.map
