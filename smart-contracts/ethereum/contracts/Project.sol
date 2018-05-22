8a116230465c2f79718c2144231ec3c3126e1fea
Jordan Brooks <jordbrooks14@gmail.com>
2018-05-22 11:43:00 -0700

    Add function to update pendingTimeline.

--- smart-contracts/ethereum/contracts/Project.sol
+++ smart-contracts/ethereum/contracts/Project.sol
@@ -5,7 +5,6 @@ contract Project {
         string title;
         string description;
         string[] conditions;
-        uint percentage; // TODO - Figure out how to represent this
         bool isComplete;
     }
 
@@ -25,6 +24,7 @@ contract Project {
     uint public contributionGoal;
     ProjectTimeline timeline;
     ProjectTimeline[] timelineHistory;
+    ProjectTimeline pendingTimeline;
 
     constructor(address _fundingService, uint _id, string _title, string _description, string _about, uint _developerId, uint _contributionGoal) public {
         fundingService = _fundingService;
@@ -35,6 +35,39 @@ contract Project {
         about = _about;
         developerId = _developerId;
         contributionGoal = _contributionGoal;
+    }
+
+    function proposeTimelineUpdate(string[] titles, string[] descriptions, string[][] conditions, uint[] percentages, bool[] isCompletes) public {
+        require(titles.length == descriptions.length);
+        require(descriptions.length == conditions.length);
+        require(conditions.length == percentages.length);
+        require(percentages.length == isCompletes.length);
+
+        ProjectTimeline proposedTimeline;
+
+        // new timeline must begin with already completed milestones from previous timeline
+        for (uint i = 0; i < timeline.milestones.length; i++) {
+            if (!timeline.milestones[i].isComplete) {
+                break;
+            } else {
+                proposedTimeline.push(timeline.milestones[i]);
+            }
+        }
+
+        // add newly proposed milestones
+        for (uint i = 0; i < titles.length; i++) {
+            ProjectMilestone newMilestone = ProjectMilestone({
+                title: titles[i],
+                description: descriptions[i],
+                conditions: conditions[i],
+                percentage: percentages[i],
+                isComplete: isCompletes[i]
+            });
+
+            proposedTimeline.push(newMilestone);
+        }
+
+        pendingTimeline = proposedTimeline;
     }
 
     function() public payable { }
