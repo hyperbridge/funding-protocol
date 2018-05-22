0df6615797535a846b5226348aba3b4a4aefa4fc
Jordan Brooks <jordbrooks14@gmail.com>
2018-05-22 10:23:34 -0700

    Add member variable status to Project.

--- smart-contracts/ethereum/contracts/Project.sol
+++ smart-contracts/ethereum/contracts/Project.sol
@@ -13,20 +13,23 @@ contract Project {
         ProjectMilestone[] milestones;
     }
 
+    enum Statuses {Draft, Pending, Published, Removed, Rejected}
+
     address public fundingService;
     uint public id;
+    Statuses public status;
     string public title;
     string public description;
     string public about;
     uint public developerId;
     uint public contributionGoal;
-
     ProjectTimeline timeline;
     ProjectTimeline[] timelineHistory;
 
     constructor(address _fundingService, uint _id, string _title, string _description, string _about, uint _developerId, uint _contributionGoal) public {
         fundingService = _fundingService;
         id = _id;
+        status = Statuses.Draft;
         title = _title;
         description = _description;
         about = _about;
