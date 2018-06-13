3502284d4564b134e075a84a0aa511b467fa2335
Walid Al Habboul <walid.wah@gmail.com>
2018-06-12 17:53:36 -0700

    adding ERC20 support, events and changing dpericated functionality

--- smart-contracts/ethereum/contracts/FundingVault.sol
+++ smart-contracts/ethereum/contracts/FundingVault.sol
@@ -1,4 +1,23 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
+
+
+// TODO move ERC20 interface to a separate file
+
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) public constant returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
 
 contract FundingVault {
 
@@ -14,35 +33,63 @@ contract FundingVault {
 
   modifier activeOnly() {
     require(active == true);
+    _;
   }
 
   address fundingService;
 
   address owner;
 
+  //TODO import pausable?
   bool active;
 
-  function FundingVault(address _fundingService) {
-    owner = msg.sender;
+  constructor(address _owner, address _fundingService) public {
+    owner = _owner;
     setFundingServiceContract(_fundingService);
+    setActive(true);
   }
 
-  function setFundingServiceContract(address _fundingService) ownerOnly {
+  function setFundingServiceContract(address _fundingService) public ownerOnly {
     fundingService = _fundingService;
   }
 
-  function setActive(bool val) ownerOnly {
+  function setActive(bool val) public ownerOnly {
     active = val;
   }
 
-  function deposite() payable {
+  event ETHDeposited(uint amount);
 
+  function depositeETH() public payable activeOnly {
+
+    emit ETHDeposited(msg.value);
   }
 
+  event ETHWithdrawn(address receiver, uint amount);
 
-  function withdraw(uint _amount, address _receiver) fundingServiceOnly activeOnly {
-    require(this.balance >= _amount);
+  function withdrawETH(uint _amount, address _receiver) public  fundingServiceOnly activeOnly {
+    require(_receiver != address(0));
+    require(_amount > 0);
+    require(getBalance() >= _amount);
+
+    emit ETHWithdrawn(_receiver, _amount);
+
     _receiver.transfer(_amount);
+  }
+
+  event ERC20Withdrawn(address ERC20, address receiver, uint amount);
+
+  function withdrawERC20(address _ERC20Adress, uint _amount, address _receiver) public fundingServiceOnly activeOnly {
+      require(_amount > 0);
+      ERC20 token = ERC20(_ERC20Adress);
+      require (token.balanceOf(this) > _amount);
+
+      emit ERC20Withdrawn(_ERC20Adress, _receiver, _amount);
+
+      require(token.transfer(_receiver,_amount));
+  }
+
+  function getBalance() public view returns(uint) {
+    return address(this).balance;
   }
 
 }
