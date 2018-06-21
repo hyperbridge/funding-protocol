ec810825f46d7bb9b604d6a81b478d7ea68f36ae
Ash Tan <ashtan19@gmail.com>
2018-06-20 18:02:42 -0700

    Bounty Service - Get Reports,Approve Hunters, release Bounty, close bounty

--- smart-contracts/ethereum/contracts/BountyService.sol
+++ smart-contracts/ethereum/contracts/BountyService.sol
@@ -1,14 +1,18 @@
 pragma solidity ^0.4.23;
 
+import "./StringUtils.sol";
+import "./SafeMath.sol";
+
 /*
 * Test createBounty Params: 1, "0xef55bfac4228981e850936aaf042951f7b146e41", "xiaxia", "Test bug description for Maple", "http://dailyhive.com"
-
+* Test submitReport Params: 0, "Test Submit Report: Look at 000"
 */
 contract BountyService {
 
     struct Bounty {
         uint bountyId;
         bytes32 targetId;           //Target of Id of the bounty (eg. Project)
+        address developerAddress;
         string bountyName;
         string bountyDescription;
         string bountyLink;          //Attach link that further describes the bounty
@@ -23,14 +27,15 @@ contract BountyService {
         uint id;
         address addr;
         string name;
-        mapping(uint => uint) BountyIdIndex; // mapping of bounty id to index in Developer.bountyIds
-        uint[] bountyIds; // the bounties belonging to this developer
+        mapping(uint => uint) BountyIdIndex;    // mapping of bounty id to index in Developer.bountyIds
+        uint[] bountyIds;                       // the bounties belonging to this developer
     }
+    
 
     address public owner;
     mapping(address => uint) public developerMap;       // address => id
     Developer[] public developers;                      // indexed by developer id
-    uint private bountyIdCounter;                       // Helps to give each bounty a unique uint ID
+    uint public bountyIdCounter;                        // Helps to give each bounty a unique uint ID
     Bounty[] bountyCollection;                          // Indexed by BountyId
 
     mapping(bytes32 => uint[]) public targetBountyMap;      //Pass the target ID and returns the bountyId's associated with it
@@ -40,6 +45,11 @@ contract BountyService {
     modifier devRestricted(uint _developerId) {
         require(developers[_developerId].id == _developerId); // check that developer exists
         require(msg.sender == developers[_developerId].addr);
+        _;
+    }
+    
+    modifier bountyStillLive(uint _bountyId) {
+        require(bountyCollection[_bountyId].isComplete == false);
         _;
     }
 
@@ -65,18 +75,19 @@ contract BountyService {
         developerMap[msg.sender] = newDeveloper.id;
     }
     
-    //TODO only allow developers to create a new bounty
-    function createBounty(uint _developerId, bytes32 _targetId, string _bountyName, string _bountyDescription, string _bountyLink) payable public{
+    function createBounty(uint _developerId, bytes32 _targetId, string _bountyName, string _bountyDescription, string _bountyLink) payable public devRestricted(_developerId){
         
         
         Bounty memory newBounty = Bounty({
             bountyId: bountyIdCounter,
             targetId: _targetId,
+            developerAddress: msg.sender,
             bountyName: _bountyName,
             bountyDescription: _bountyDescription,
             bountyLink: _bountyLink,
             bountyValue: msg.value,
-            isComplete: false
+            isComplete: false,
+            approvedHunterMap: new address[](0)
         });
         
         targetBountyMap[newBounty.targetId].push(newBounty.bountyId);
@@ -86,23 +97,69 @@ contract BountyService {
         
     }
     
-    function submitBountyReport(uint _bountyId, string _report) public {
+    function submitBountyReport(uint _bountyId, string _report) public bountyStillLive(_bountyId){
         bountyReportMap[_bountyId].push(_report);
         bountyHunterReportMap[_bountyId].push(msg.sender);
         bountyCollection[_bountyId].compensatedHunterMap[msg.sender] = false;
 
     }
     
-    // function getReportsforBounty(uint _bountyId) public view returns (string) {
-    //     string memory bountyConcatString;
-    //     for (uint i=0; i > bountyReportMap[_bountyId].length; i++) {
+    function getReportsforBounty(uint _bountyId) public view returns (string) {
+        string memory bountyConcatString;
+        for (uint i=0; i < bountyReportMap[_bountyId].length; i++) {
             
-    //         bountyConcatString = bountyReportMap[_bountyId][i] + bountyReportMap[_bountyId][i+1];
-    //     }
-    // }
+            bountyConcatString = strings.concat(strings.toSlice(bountyConcatString), strings.toSlice("#REPORT#"));
+            bountyConcatString = strings.concat(strings.toSlice(bountyConcatString), strings.toSlice(bountyReportMap[_bountyId][i]));
+        }
+        return bountyConcatString;
+    }
+    
+    
+    //Move a BountyHunter to the approved Bounty Hunter array
+    //Perform before releasing bounty
+    function approveBountyHunter(uint _bountyId, address _bountyHunter) public{
+        require(bountyCollection[_bountyId].developerAddress == msg.sender);    //Check if it is the original developer that created this bounty
 
+        bountyCollection[_bountyId].approvedHunterMap.push(_bountyHunter);
+    }
+    
+    
+    function getApprovedBountyHunters(uint _bountyId) public view returns (address[]) {
+        return bountyCollection[_bountyId].approvedHunterMap;
+    }
+    
+    
+    // Release Bounty to bounty hunters on the approvedHunterMap array of a Bounty
+    function releaseBounty(uint _bountyId, uint _developerId) public devRestricted(_developerId) {
+        require(bountyCollection[_bountyId].developerAddress == msg.sender);    //Check if it is the original developer that created this bounty
+        
+        uint bountyShare = SafeMath.div(bountyCollection[_bountyId].bountyValue, bountyCollection[_bountyId].approvedHunterMap.length);
+        
+        for (uint i=0; i< bountyCollection[_bountyId].approvedHunterMap.length; i++) {
+            bountyCollection[_bountyId].approvedHunterMap[i].transfer(bountyShare);
+        }
+    }
+    
+    function closeBounty(uint _bountyId, uint _developerId) public devRestricted(_developerId) bountyStillLive(_bountyId) {
+        require(bountyCollection[_bountyId].developerAddress == msg.sender);
+        
+        bountyCollection[_bountyId].isComplete = true;
+    }
+    
+    
+    function getBountyStatus(uint _bountyId) public view returns (bool) {
+        return bountyCollection[_bountyId].isComplete;
+    }
+    
+    function getBountyValue(uint _bountyId) public view returns (uint) {
+        return bountyCollection[_bountyId].bountyValue;
+    }
+    
 
 
 
     function() public payable { }
 }
+
+
+
