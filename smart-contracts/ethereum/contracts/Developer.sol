2ca3318224baab52a2f39fb948b454a6235c2279
Jordan Brooks <jordbrooks14@gmail.com>
2018-07-12 12:50:27 -0700

    Developer contract containing logic for manipulation of FundingStorage with developer-specific data.

---
+++ smart-contracts/ethereum/contracts/Developer.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.24;
+
+import "./libraries/DeveloperStorageAccess.sol";
+
+contract Developer {
+
+    using DeveloperStorageAccess for address;
+
+    modifier fundingServiceOnly() {
+        require(msg.sender == fundingService, "This action can only be performed by the Funding Service.");
+        _;
+    }
+
+    modifier validFundingContractOnly() {
+        // TODO
+        _;
+    }
+
+    address public fundingService;
+    address public fundingStorage;
+
+    constructor(address _fundingService, address _fundingStorage) public {
+        fundingService = _fundingService;
+        fundingStorage = _fundingStorage;
+
+        // reserve developerId 0
+        fundingStorage.incrementNextId();
+    }
+
+    function () public payable {
+        revert();
+    }
+
+    function createDeveloper(string _name, address _devAddress) external fundingServiceOnly {
+        require(fundingStorage.getDeveloperId(_address) == 0, "This account is already a developer.");
+
+        // Get next ID from storage + increment next ID
+        uint id = fundingStorage.generateNewId();
+
+        // Create developer
+        fundingStorage.setDeveloperId(msg.sender, id);
+        fundingStorage.setName(id, _name);
+        fundingStorage.setAddress(id, msg.sender);
+    }
+
+    function getDeveloper(uint _id) external view fundingServiceOnly
+        returns (
+            uint id,
+            address addr,
+            string name,
+            uint reputation,
+            uint[] ownedProjectIds
+        )
+    {
+        DeveloperStorageAccess.Developer memory developer = pStorage.getDeveloper(_id);
+        return (_id, developer.addr, developer.name, developer.reputation, developer.ownedProjectIds);
+    }
+
+    function updateDeveloperReputation(uint _developerId, uint _val) validFundingContractOnly {
+        uint currentRep = fundingStorage.getReputation(_developerId);
+        fundingStorage.setReputation(_developerId, currentRep + _val);
+    }
+}
