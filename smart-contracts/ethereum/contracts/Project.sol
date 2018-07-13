4be22a92e65899f93eccc6802c3db4294bd9834e
Jordan Brooks <jordbrooks14@gmail.com>
2018-07-13 13:42:50 -0700

    Decoupled Project from FundingService.

--- smart-contracts/ethereum/contracts/Project.sol
+++ smart-contracts/ethereum/contracts/Project.sol
@@ -1,17 +1,20 @@
 pragma solidity ^0.4.24;
 
-import "./FundingService.sol";
-import "./ProjectEternalStorage.sol";
-import "./libraries/ProjectStorageAccess.sol";
-import "./libraries/ProjectLib.sol";
-import "./libraries/ProjectTimelineLib.sol";
-import "./libraries/ProjectContributionTierLib.sol";
-import "./libraries/ProjectTimelineProposalLib.sol";
-import "./libraries/ProjectMilestoneCompletionLib.sol";
+import "./FundingStorage.sol";
+import "./libraries/storage/ProjectStorageAccess.sol";
+import "./libraries/storage/DeveloperStorageAccess.sol";
+import "./libraries/storage/ContributionStorageAccess.sol";
+import "./libraries/project/ProjectLib.sol";
+import "./libraries/project/ProjectTimelineLib.sol";
+import "./libraries/project/ProjectContributionTierLib.sol";
+import "./libraries/project/ProjectTimelineProposalLib.sol";
+import "./libraries/project/ProjectMilestoneCompletionLib.sol";
 
 contract Project {
 
     using ProjectStorageAccess for address;
+    using DeveloperStorageAccess for address;
+    using ContributionStorageAccess for address;
 
     using ProjectLib for address;
     using ProjectTimelineLib for address;
@@ -20,72 +23,70 @@ contract Project {
     using ProjectMilestoneCompletionLib for address;
 
     enum Status {Draft, Pending, Published, Removed, Rejected}
+    
+    address fundingStorage;
 
-    // TODO - rethink modifiers
-    //    modifier devRestricted() {
-    //        require(msg.sender == developer, "Caller is not the developer of this project.");
-    //        _;
-    //    }
-    //
-    //    modifier contributorRestricted() {
-    //        FundingService fs = FundingService(fundingService);
-    //        require(fs.projectContributionAmount(this, msg.sender) != 0, "Caller is not a contributor to this project.");
-    //        _;
-    //    }
+    event ProjectCreated(uint projectId);
 
-    modifier fundingServiceRestricted() {
-        require(msg.sender == fundingService, "This action can only be performed by the Funding Service.");
-        _;
-    }
-
-    address fundingService;
-    address pStorage;
-
-    constructor(address _fundingService) public {
-        fundingService = _fundingService;
-    }
-
-    function registerProjectStorage(address _projectStorage) public {
-        pStorage = _projectStorage;
+    constructor(address _fundingStorage) public {
+        fundingStorage = _fundingStorage;
     }
 
     function createProject(
         string _title,
         string _description,
         string _about,
-        uint _contributionGoal,
-        address _developer,
-        uint _developerId
+        uint _contributionGoal
     )
-    public
-    fundingServiceRestricted
-    returns (uint)
+        external
     {
-        uint id = pStorage.createProject(_title, _description, _about, _contributionGoal, uint(Status.Draft), _developer, _developerId);
-        return id;
+        uint developerId = fundingStorage.getDeveloperId(msg.sender);
+        require(developerId != 0, "This address is not a developer.");
+
+        uint id = fundingStorage.createProject(_title, _description, _about, _contributionGoal, uint(Status.Draft), msg.sender, developerId);
+        emit ProjectCreated(id);
     }
 
-    function getProject(uint _projectId) public view returns (bool isActive, uint status, string title, string description, string about, uint contributionGoal, address developer, uint developerId) {
-        isActive = pStorage.getProjectIsActive(_projectId);
-        status = pStorage.getStatus(_projectId);
-        title = pStorage.getTitle(_projectId);
-        description = pStorage.getDescription(_projectId);
-        about = pStorage.getAbout(_projectId);
-        contributionGoal = pStorage.getContributionGoal(_projectId);
-        developer = pStorage.getDeveloper(_projectId);
-        developerId = pStorage.getDeveloperId(_projectId);
+    function getProject(
+        uint _projectId
+    )
+        external
+        view
+        returns (
+            bool isActive,
+            uint status,
+            string title,
+            string description,
+            string about,
+            uint contributionGoal,
+            bool noRefunds,
+            bool noTimeline,
+            address developer,
+            uint developerId
+        )
+    {
+        isActive = fundingStorage.getProjectIsActive(_projectId);
+        status = fundingStorage.getStatus(_projectId);
+        title = fundingStorage.getTitle(_projectId);
+        description = fundingStorage.getDescription(_projectId);
+        about = fundingStorage.getAbout(_projectId);
+        contributionGoal = fundingStorage.getContributionGoal(_projectId);
+        developer = fundingStorage.getDeveloper(_projectId);
+        noRefunds = fundingStorage.getNoRefunds(_projectId);
+        noTimeline = fundingStorage.getNoTimeline(_projectId);
+        developerId = fundingStorage.getDeveloperId(_projectId);
 
-        return (isActive, status, title, description, about, contributionGoal, developer, developerId);
+        return (isActive, status, title, description, about, contributionGoal, noRefunds, noTimeline, developer, developerId);
     }
 
-    function getProjects() public view returns (uint[]) {
-        uint numProjects = pStorage.getNextId();
+    function getProjects() external view returns (uint[]) {
+        uint numProjects = fundingStorage.getNextId();
 
         uint[] memory activeProjects = new uint[](numProjects);
 
         uint length = 0;
         for (uint i = 0; i < numProjects; i++) {
-            if (pStorage.getProjectIsActive(i)) {
+            if (fundingStorage.getProjectIsActive(i)) {
                 activeProjects[length] = (i);
                 length++;
             }
@@ -101,11 +102,11 @@ contract Project {
         string _description,
         uint _percentage
     )
-    public
+        external
     {
         require(_percentage <= 100, "Milestone percentage cannot be greater than 100.");
 
-        pStorage.addMilestone(_projectId, _isPending, _title, _description, _percentage);
+        fundingStorage.addMilestone(_projectId, _isPending, _title, _description, _percentage);
     }
 
     function editMilestone(
@@ -116,48 +117,47 @@ contract Project {
         string _description,
         uint _percentage
     )
-    public
+        external
     {
         require(_percentage <= 100, "Milestone percentage cannot be greater than 100.");
 
-        pStorage.editMilestone(_projectId, _isPending, _index, _title, _description, _percentage);
+        fundingStorage.editMilestone(_projectId, _isPending, _index, _title, _description, _percentage);
     }
 
-    function clearPendingTimeline(uint _projectId) public {
-        pStorage.clearPendingTimeline(_projectId);
+    function clearPendingTimeline(uint _projectId) external {
+        fundingStorage.clearPendingTimeline(_projectId);
     }
 
-    function submitProjectForReview(uint _projectId) public { // devRestricted(_developerId) {
-        pStorage.submitProjectForReview(_projectId);
+    function submitProjectForReview(uint _projectId) external {
+        fundingStorage.submitProjectForReview(_projectId);
     }
 
-    function proposeNewTimeline(uint _projectId) public {
-        pStorage.proposeNewTimeline(_projectId);
+    function proposeNewTimeline(uint _projectId) external {
+        fundingStorage.proposeNewTimeline(_projectId);
     }
 
-    function voteOnTimelineProposal(uint _projectId, bool _approved) public {
-        pStorage.voteOnTimelineProposal(_projectId, _approved);
+    function voteOnTimelineProposal(uint _projectId, bool _approved) external {
+        fundingStorage.voteOnTimelineProposal(_projectId, _approved);
     }
 
-    function finalizeTimelineProposal(uint _projectId) public {
+    function finalizeTimelineProposal(uint _projectId) external {
         // TimelineProposal must be active
-        require(pStorage.getTimelineProposalIsActive(_projectId), "There is no timeline proposal active.");
+        require(fundingStorage.getTimelineProposalIsActive(_projectId), "There is no timeline proposal active.");
 
         if (hasPassedTimelineProposalVote(_projectId)) {
-            pStorage.succeedTimelineProposal(_projectId);
+            fundingStorage.succeedTimelineProposal(_projectId);
         } else {
             // Timeline proposal has failed
-            pStorage.setTimelineProposalHasFailed(_projectId, true);
+            fundingStorage.setTimelineProposalHasFailed(_projectId, true);
         }
     }
 
     function hasPassedTimelineProposalVote(uint _projectId) private view returns (bool) {
-        FundingService fs = FundingService(fundingService);
-        uint numContributors = fs.getProjectContributorList(_projectId).length;
-        uint approvalCount = pStorage.getTimelineProposalApprovalCount(_projectId);
-        uint disapprovalCount = pStorage.getTimelineProposalDisapprovalCount(_projectId);
+        uint numContributors = fundingStorage.getProjectContributorListLength(_projectId);
+        uint approvalCount = fundingStorage.getTimelineProposalApprovalCount(_projectId);
+        uint disapprovalCount = fundingStorage.getTimelineProposalDisapprovalCount(_projectId);
         uint numVoters = approvalCount + disapprovalCount;
-        bool isTwoWeeksLater = now >= pStorage.getTimelineProposalTimestamp(_projectId) + 2 weeks;
+        bool isTwoWeeksLater = now >= fundingStorage.getTimelineProposalTimestamp(_projectId) + 2 weeks;
         uint votingThreshold = numVoters * 75 / 100;
 
         // Proposal needs >75% total approval, or for 2 weeks to have passed and >75% approval among voters
@@ -167,33 +167,32 @@ contract Project {
         return ((approvalCount > numContributors * 75 / 100) || (approvalCount > votingThreshold));
     }
 
-    function submitMilestoneCompletion(uint _projectId, string _report) public {
-        pStorage.submitMilestoneCompletion(_projectId, _report);
+    function submitMilestoneCompletion(uint _projectId, string _report) external {
+        fundingStorage.submitMilestoneCompletion(_projectId, _report);
     }
 
-    function voteOnMilestoneCompletion(uint _projectId, bool _approved) public {
-        pStorage.voteOnMilestoneCompletion(_projectId, _approved);
+    function voteOnMilestoneCompletion(uint _projectId, bool _approved) external {
+        fundingStorage.voteOnMilestoneCompletion(_projectId, _approved);
     }
 
-    function finalizeMilestoneCompletion(uint _projectId) public {
+    function finalizeMilestoneCompletion(uint _projectId) external {
         // MilestoneCompletionSubmission must be active
-        require(pStorage.getMilestoneCompletionSubmissionIsActive(_projectId), "No vote on milestone completion active.");
+        require(fundingStorage.getMilestoneCompletionSubmissionIsActive(_projectId), "No vote on milestone completion active.");
 
         if (hasPassedMilestoneCompletionVote(_projectId)) {
-            pStorage.succeedMilestoneCompletion(_projectId);
+            fundingStorage.succeedMilestoneCompletion(_projectId);
         } else {
             // Set milestone completion submission has failed
-            pStorage.setMilestoneCompletionSubmissionHasFailed(_projectId, true);
+            fundingStorage.setMilestoneCompletionSubmissionHasFailed(_projectId, true);
         }
     }
 
-    function hasPassedMilestoneCompletionVote(uint _projectId) private view returns (bool) {
-        FundingService fs = FundingService(fundingService);
-        uint numContributors = fs.getProjectContributorList(_projectId).length;
-        uint approvalCount = pStorage.getMilestoneCompletionSubmissionApprovalCount(_projectId);
-        uint disapprovalCount = pStorage.getMilestoneCompletionSubmissionDisapprovalCount(_projectId);
+    function hasPassedMilestoneCompletionVote(uint _projectId) external view returns (bool) {
+        uint numContributors = fundingStorage.getProjectContributorListLength(_projectId);
+        uint approvalCount = fundingStorage.getMilestoneCompletionSubmissionApprovalCount(_projectId);
+        uint disapprovalCount = fundingStorage.getMilestoneCompletionSubmissionDisapprovalCount(_projectId);
         uint numVoters = approvalCount + disapprovalCount;
-        bool isTwoWeeksLater = now >= pStorage.getMilestoneCompletionSubmissionTimestamp(_projectId) + 2 weeks;
+        bool isTwoWeeksLater = now >= fundingStorage.getMilestoneCompletionSubmissionTimestamp(_projectId) + 2 weeks;
         uint votingThreshold = numVoters * 75 / 100;
 
         // Proposal needs >75% total approval, or for 2 days to have passed and >75% approval among voters
@@ -203,8 +202,16 @@ contract Project {
         return ((approvalCount > numContributors * 75 / 100) || (approvalCount > votingThreshold));
     }
 
-    function addTier(uint _projectId, uint _contributorLimit, uint _maxContribution, uint _minContribution, string _rewards) public {
-        pStorage.addTier(_projectId, _contributorLimit, _maxContribution, _minContribution, _rewards);
+    function addTier(
+        uint _projectId,
+        uint _contributorLimit,
+        uint _maxContribution,
+        uint _minContribution,
+        string _rewards
+    )
+        external
+    {
+        fundingStorage.addTier(_projectId, _contributorLimit, _maxContribution, _minContribution, _rewards);
     }
 
     function editTier(
@@ -215,12 +222,12 @@ contract Project {
         uint _minContribution,
         string _rewards
     )
-    public
+        external
     {
-        pStorage.setPendingContributionTier(_projectId, _index, _contributorLimit, _maxContribution, _minContribution, _rewards);
+        fundingStorage.setPendingContributionTier(_projectId, _index, _contributorLimit, _maxContribution, _minContribution, _rewards);
     }
 
-    function finalizeTiers(uint _projectId) public {
-        pStorage.finalizeTiers(_projectId);
+    function finalizeTiers(uint _projectId) external {
+        fundingStorage.finalizeTiers(_projectId);
     }
 }
