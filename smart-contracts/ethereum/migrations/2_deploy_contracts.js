861cb757d95a1802c5d08525b896fe633781047a
Jordan Brooks <jordbrooks14@gmail.com>
2018-07-21 00:16:25 -0700

    Curation to deploy script.

--- smart-contracts/ethereum/migrations/2_deploy_contracts.js
+++ smart-contracts/ethereum/migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@ const FundingStorage = artifacts.require("FundingStorage");
 const ProjectStorageAccess = artifacts.require("ProjectStorageAccess");
 const DeveloperStorageAccess = artifacts.require("DeveloperStorageAccess");
 const ContributionStorageAccess = artifacts.require("ContributionStorageAccess");
+const CurationStorageAccess = artifacts.require("CurationStorageAccess");
 
 const ProjectBase = artifacts.require("ProjectBase");
 const ProjectRegistration = artifacts.require("ProjectRegistration");
@@ -16,18 +17,23 @@ const Contribution = artifacts.require("Contribution");
 
 const Developer = artifacts.require("Developer");
 
+const Curation = artifacts.require("Curation");
+
 async function doDeploy(deployer, network) {
     await deployer.deploy(FundingStorage);
     const fs = await FundingStorage.deployed();
 
     await deployer.deploy(ProjectStorageAccess);
-    await deployer.link(ProjectStorageAccess, [ProjectBase, ProjectHelpersLibrary, Contribution]);
+    await deployer.link(ProjectStorageAccess, [ProjectBase, ProjectHelpersLibrary, Contribution, Curation]);
 
     await deployer.deploy(DeveloperStorageAccess);
     await deployer.link(DeveloperStorageAccess, [Developer, ProjectBase]);
 
     await deployer.deploy(ContributionStorageAccess);
     await deployer.link(ContributionStorageAccess, [Contribution, ProjectBase]);
+
+    await deployer.deploy(CurationStorageAccess);
+    await deployer.link(CurationStorageAccess, Curation);
 
     await deployer.deploy(ProjectHelpersLibrary);
     await deployer.link(ProjectHelpersLibrary, [ProjectTimelineProposal, ProjectRegistration, ProjectMilestoneCompletion]);
@@ -40,6 +46,7 @@ async function doDeploy(deployer, network) {
 
     await deployer.deploy(Developer);
     await deployer.deploy(Contribution);
+    await deployer.deploy(Curation);
 }
 
 module.exports = function(deployer, network) {
